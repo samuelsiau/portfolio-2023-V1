80569cf417ee04e1cbed52946fcaa771628d19f6
Samuel Siau <samuelsiau@gmail.com>
2023-04-29 00:34:40 +1200

    213131

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: './docs'
+  outDir: './docs',
+  build: {
+    assets: 'assets'
+  }
 });
