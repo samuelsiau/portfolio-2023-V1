b79ff9c3279aab736fa497bb354dbbf6a9a9fae9
Samuel Siau <samuelsiau@gmail.com>
2023-04-28 21:32:41 +1200

    1231231

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samuelsiau.github.io',
-  base: 'https://samuelsiau.github.io/portfolio-2023-V1',
+  base: '/portfolio-2023-V1',
   outDir: './docs'
 });
