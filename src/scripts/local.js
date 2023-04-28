5802d039cef0d456a3b0b773a934b87a97f9c43c
Samuel Siau <samuelsiau@gmail.com>
2023-04-29 02:47:50 +1200

    hello world

--- src/scripts/local.js
+++ src/scripts/local.js
@@ -1,3 +1,39 @@
+gsap.registerPlugin(CustomEase);
+
 const header = document.querySelector(".nav-wrapper");
 const headroom = new Headroom(header);
 headroom.init();
+
+const heroHeading = document.querySelector('.animateHeroText'); 
+const splitType = new SplitType(heroHeading, {types: 'lines'});
+
+// $('.animateHeroText .line').wrap('<div class="overflow-hidden"></div>');
+
+// ScrollReveal().reveal('.animateHeroText .line', { 
+//   distance: '50px',
+//   interval: 420,
+//   reset: true
+// });
+
+CustomEase.create("hop", "0.5, 0, 0, 1");
+
+const homeHeroText = gsap.from('.animateHeroText .line', {
+  opacity: 0,
+  y: 50,
+  duration: 1.2,
+  ease: "hop",
+  stagger: { amount: 1.2 },
+}).then(displayCTAFunction);
+
+const animateHomeCTA =  gsap.from('.heroHome .cta',{
+  opacity: 0,
+  y: 50,
+  duration: 1.2,
+  ease: "hop"
+})
+
+animateHomeCTA.pause();
+
+function displayCTAFunction() {
+  animateHomeCTA.play();
+}
