528e34258df8e4e681c55269479a53770c531b3f
Samuel Siau <samuelsiau@gmail.com>
2023-05-24 20:11:11 +1200

    updated

--- src/scripts/local.js
+++ src/scripts/local.js
@@ -61,13 +61,18 @@
 const btnBacktoTop1 = $('.backtotop1');
 const btnBacktoTop2 = $('.backtotop2');
 
-const scrolltotopgsap = gsap.to(window, {
-  duration: 0.2,
-  scrollTo: '0'
+const backToTopAnimation = gsap.to(window, {
+  duration: 0.1,
+  scrollTo: "0",
+  ease: "none"
 })
 
-btnBacktoTop2.click(function(){
-  scrolltotopgsap.restart();
+btnBacktoTop2.on("click",function(){
+  backToTopAnimation.restart();
+})
+
+btnBacktoTop1.click(function(){
+  backToTopAnimation.restart();
 })
 
 
