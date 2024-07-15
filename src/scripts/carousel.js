07639ff57341df52a918e3841bffbe6c034075f8
samuelsiau <samuelsiau@gmail.com>
2024-07-16 10:49:00 +1200

    Updated

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,9 +1,42 @@
-var $carousel = $('.carousel').flickity({
-    // options
-    cellAlign: 'center',
-    freeScroll: true,
-    // contain: true,
-    // percentPosition: false,
-    // prevNextButtons: false,
-    pageDots: false
-});
+
+// const $carousel = $('.carousel').flickity({
+//     // options
+//     cellAlign: 'center',
+//     freeScroll: true,
+//     // contain: true,
+//     // percentPosition: false,
+//     // prevNextButtons: false,
+//     pageDots: false
+// });
+
+
+$('.carousel-container').each( function( i, container ) {
+    var $container = $( container );
+  
+    var $carousel = $container.find('.carousel').flickity({
+      cellSelector: '.carousel-cell',
+      cellAlign: "center",
+      freeScroll: true,
+      imagesLoaded: true,
+      percentPosition: false,
+      pageDots: false
+    });
+
+    // Flickity instance
+    var flkty = $carousel.data('flickity');
+    // console.log(flkty.selectedIndex);
+
+    
+    
+    // // previous
+    // var $prev = $container.find('.prev');
+    // $prev.on( 'click', function() {
+    //     $carousel.flickity('previous', false);
+    // });
+    // // next
+    // var $next = $container.find('.next');
+    // $next.on( 'click', function() {
+    //     $carousel.flickity('next', false);
+    // });
+
+  });
